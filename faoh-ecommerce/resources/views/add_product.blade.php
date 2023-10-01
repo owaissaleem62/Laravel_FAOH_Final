2fae642a42b28617b97441cb2869c7588f8c7ae5
aliraza2023 <aliraza2209f@aptechgdn.net>
2023-10-01 16:08:38 +0500

    no update

--- faoh-ecommerce/resources/views/add_product.blade.php
+++ faoh-ecommerce/resources/views/add_product.blade.php
@@ -5,8 +5,6 @@
         <div class="card-header py-3 card shadow mb-4">
         <h6 class="m-0 font-weight-bold text-primary">ADD PRODUCTS</h6>
     </div>
-
-
 <div class="col-md-6 offset-md-2">
     <!-- Main Content -->
     @if (session('status'))
@@ -54,9 +52,6 @@
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
-
-
-
 
 </div>
 <!-- /.container-fluid -->
